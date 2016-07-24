f3e9f962ebcdd23f2cf099a0583f8a96ea74bc0d
TakaseNochi <m5201142uaizu@gmail.com>
2016-07-24 14:09:11 +0900

    first commit

---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.lang.*;
+import java.net.*;
+import java.util.*;
+
+public class Hello{
+    
+    public Hello(){
+    }
+    
+    public static void main(String[] args){
+        
+        System.out.println("Hello World");
+        
+    }
+}
